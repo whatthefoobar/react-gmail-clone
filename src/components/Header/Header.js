deca40acf3aba60bd02825a245cabfb68b50abde
whatthefoobar <irina.mntn@gmail.com>
2022-05-08 17:38:06 +0200

    logout on click of the img avatar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,19 @@ import SearchIcon from '@mui/icons-material/Search';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import AppsIcon from '@mui/icons-material/Apps';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../../features/userSlice';
+import { auth } from '../../firebase';
 
 function Header() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -31,11 +42,22 @@ function Header() {
         <IconButton>
           <AppsIcon />
         </IconButton>
+
         <IconButton>
           <NotificationsIcon />
         </IconButton>
         <div className="header__right__avatar">
-          <Avatar />
+          <Avatar
+            style={{
+              marginTop: '7px',
+              height: '30px',
+              width: '30px',
+              display: 'flex',
+            }}
+            onClick={signOut}
+            src={user?.photoUrl}
+            alt
+          />
         </div>
       </div>
     </div>
