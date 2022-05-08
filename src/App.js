21d415676dae8ac28e5cc21d069a7230a25ce5e7
whatthefoobar <irina.mntn@gmail.com>
2022-05-08 17:29:28 +0200

    auth w google is done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -6,26 +6,51 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Mail from './components/Mail/Mail';
 import EmailList from './components/EmailList/EmailList';
 import SendMail from './components/SendMail/SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { login, selectUser } from './features/userSlice';
+import { auth } from './firebase';
+import Login from './components/Login/Login';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // the user is logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      }
+    });
+  }, []);
+
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Routes>
-            <Route path="/mail" element={<Mail />} />
-            <Route path="/" element={<EmailList />} />
-          </Routes>
-        </div>
-        {sendMessageIsOpen && <SendMail />}
-        {/* we toggle the send mail component by checking the sendMessageIsOpen from
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Routes>
+              <Route path="/mail" element={<Mail />} />
+              <Route path="/" element={<EmailList />} />
+            </Routes>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
+          {/* we toggle the send mail component by checking the sendMessageIsOpen from
         false to true */}
-      </div>
+        </div>
+      )}
     </Router>
   );
 }
